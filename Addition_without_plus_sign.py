8345587c08d4432b6f6428afb35979262d434940
mrmgobir <144796863+mrmgobir@users.noreply.github.com>
2023-09-13 17:05:41 +0100

    Add files via upload
    This is a program that shows you how to sum two numbers without using the "+" sign. 
    The two methods used are: 
    for loop and function.

---
+++ Addition_without_plus_sign.py
@@ -0,0 +1,13 @@
+# first method:
+
+def addition (a, b):
+    print(f"the sum of {a} and {b} is",  sum({a, b}) )
+
+addition(40, 70)
+
+# second method
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# total = 0
+# for num in numbers:
+#     total -= -num
+# print("the sum of the numbers is",  total)
